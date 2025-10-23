e889856595285b42c2ab7bbb66320c81eefb899d
qhgill <qhgill2@gmail.com>
2025-10-22 20:32:05 -0700

    image blur

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -3,11 +3,12 @@ import "@/styles/SplitBackground.css";
 import "@/styles/button.css";
 import "@/styles/book.css";
 import "@/styles/bookmark.css";
+import "@/styles/classroom.css"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <main className="p-8">
+      <main className="">
         <Component {...pageProps} />
       </main>
     </>
