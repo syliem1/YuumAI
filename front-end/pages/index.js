9605b5399decdd69d59851fb59fa460f8be2e514
Ryan Taheri <politeforce@csu.fullerton.edu>
2025-11-10 01:19:11 -0800

    intro page done

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -15,14 +15,28 @@ export default function Home() {
 
       {/* Content */}
       <div className="detention_card">
-        <div className="riotID_box">
-          Hello
-        </div>
+        <input type="text" className="riotID_box" placeholder="Riot ID">
+        </input>
+        <input type="text" className="tagline_box" placeholder="1234">
+        </input>
+        <input type="number" className="num_games_analyzed_box" placeholder="10">
+        </input>
           {/* <Link href="/FlipBook" className="text-background hover:underline">
             <button className="rounded-lg bg-[#8b6f4e] px-4 py-2 text-white transition hover:bg-[#73583f]">
               Search
             </button>
           </Link> */}
+        <button
+            className="magical-button"
+        >
+            <span className="button-text">Submit</span>
+                <div className="particles">
+                    <span className="particle"></span>
+                    <span className="particle"></span>
+                    <span className="particle"></span>
+                    <span className="particle"></span>
+                </div>
+          </button>
       </div>
     </section>
   );
